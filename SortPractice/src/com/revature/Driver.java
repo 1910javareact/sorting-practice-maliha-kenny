1b6fc7f7febb5d036edb105eb1a4b7aa7a65ab1c
Kenneth Davis <kennethdavis391@gmail.com>
2019-11-05 13:36:10 -0500

    sorting done

--- SortPractice/src/com/revature/Driver.java
+++ SortPractice/src/com/revature/Driver.java
@@ -8,7 +8,7 @@ import com.revature.sort.MergeSort;
 
 public class Driver {
 	
-	private static final int AMOUNT = 5;
+	private static final int AMOUNT = 100000;
 
 	public static void main(String[] args) {
 		int[] arr = new int[AMOUNT];
@@ -17,9 +17,7 @@ public class Driver {
 		for(int i = 0; i < AMOUNT; i++) {
 			arr[i] = r.nextInt(AMOUNT);
 		}
-		
-		System.out.println(Arrays.toString(arr));
-		
+				
 		int[] arr2 = Arrays.copyOf(arr, AMOUNT);
 		
 		long t1 = System.nanoTime();
@@ -41,7 +39,7 @@ public class Driver {
 		
 		if(Arrays.equals(arr, arr2)) {
 			System.out.println("It took bubblesort algorithm " + bubblesort + " ns to sort");
-			System.out.println("It took mergesort algorithm " + mergesort + " ns to sort");
+			System.out.println("It took mergesort algorithm  " + mergesort + " ns to sort");
 		} else {
 			System.out.println("Arrays are not the same.");
 		}
--- SortPractice/src/com/revature/sort/MergeSort.java
+++ SortPractice/src/com/revature/sort/MergeSort.java
@@ -1,12 +1,49 @@
 package com.revature.sort;
 
+import java.util.Arrays;
+
 public class MergeSort {
 	
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+		if(arr.length == 1) {
+			return;
+		}else if(arr.length == 2){
+			if (arr[0] > arr[1]) {
+				int temp = arr[0];
+				arr[0] = arr[1];
+				arr[1] = temp;
+				return;
+			}
+		}else {
+			int[] leftArr = new int[arr.length/2];
+			int[] rightArr = new int[arr.length - arr.length/2];
+			
+			for(int i = 0; i < arr.length/2; i++) {
+				leftArr[i] = arr[i];
+			}
+			
+			for(int i = arr.length/2; i < arr.length; i++) {
+				rightArr[i - arr.length/2] = arr[i];
+			}
+				
+			sort(leftArr);
+			sort(rightArr);
+			merge(arr, leftArr, rightArr);
+		}
 	}
-	
+	//Arrays.copyOfRange(arr, 0, arr.length/2);
+	//Arrays.copyOfRange(arr, arr.length/2 + 1, arr.length - 1);
 	private static void merge(int[] arr, int[] left, int[] right) {
-		// TODO Auto-generated method stub
+		int l = 0;
+		int r = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (r >= right.length || (l < left.length && left[l] < right[r])) {
+				arr[i] = left[l];
+				l++;
+			}else{
+				arr[i] = right[r];
+				r++;
+			}
+		}
 	}
 }
